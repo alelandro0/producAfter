99f992bf525fc560235a2674525450ba21dfc0c8
alelandro0 <alejandro0giraldo@hotmail.com>
2024-03-03 23:18:34 -0500

    cambio 0303

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import '../layout/DefaultLayout.css'
-import logo from '../../public/img/logo.png';
+
 
 interface DefaultLayoutProps{
     children: React.ReactNode,
 }
 export default function DefaultLayout({children}:DefaultLayoutProps){
+    const logo ='https://firebasestorage.googleapis.com/v0/b/react-firebase-upload-480ee.appspot.com/o/ImagePublication%2FLogo-removebg-preview.png_1709518626057?alt=media&token=f2cf977d-3696-456e-99a2-ae7c602453cc'
     return(
         <>
         <header className="header-layout">
